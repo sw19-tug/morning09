4858410ddec35bfdf259c244ddde8b2deb98b3da
Guenther Moser <moser@tugraz.at>
2019-03-23 13:23:42 +0100

    feature/LANG_001 added Basic View Elements [MM,MG]

--- app/src/androidTest/java/at/tugraz/ist/swe/lang/MainAppSurfaceElementsVisibleTest.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/lang/MainAppSurfaceElementsVisibleTest.java
@@ -1,9 +1,12 @@
 package at.tugraz.ist.swe.lang;
 
+
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -11,7 +14,6 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,6 +22,10 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class MainAppSurfaceElementsVisibleTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+
     @Test
     public void testButtonsVisible() {
 

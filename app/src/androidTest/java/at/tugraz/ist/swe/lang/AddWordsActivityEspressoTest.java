f6b7250f495a96b0cc04e5edf6fb0bdbac94c632
Daniel Gärber <daniel.gaerber@student.tugraz.at>
2019-03-28 10:03:15 +0100

    LANG_001 AddWordsActivity test included toast messages and listView elements [GD,SC]

--- app/src/androidTest/java/at/tugraz/ist/swe/lang/AddWordsActivityEspressoTest.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/lang/AddWordsActivityEspressoTest.java
@@ -10,39 +10,98 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.anything;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static org.hamcrest.core.IsNot.not;
 
 @RunWith(AndroidJUnit4.class)
 public class AddWordsActivityEspressoTest {
     @Rule
     public ActivityTestRule<AddWordsActivity> addWordsActivityTestRule = new ActivityTestRule<>(AddWordsActivity.class);
 
+    String toast = "Empty input!";
 
     @Test
-    public void testAddButtonVisible() {
+    public void testElementsVisible() {
 
-        onView(withId(R.id.btAdd)).check(matches(isDisplayed()));
-        onView(withId(R.id.btAdd)).check(matches(withText("ADD")));
-    }
-
-    @Test
-    public void testAddEnglishVisible() {
+        onView(withId(R.id.btnAdd)).check(matches(isDisplayed()));
+        onView(withId(R.id.btnAdd)).check(matches(withText("ADD")));
         onView(withId(R.id.ptAddEnglish)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void testAddEnglishVisible() {
         onView(withId(R.id.ptAddGerman)).check(matches(isDisplayed()));
+        onView(withId(R.id.lvWordList)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void testWordListVisible() {
-        onView(withId(R.id.lvWordList)).check(matches(isDisplayed()));
+    public void testAddWordsVisible() {
+        String inputEnglish = "Apple";
+        String inputGerman = "Apfel";
+        String display = inputEnglish + " : " + inputGerman;
+
+        onView(withId(R.id.ptAddEnglish)).perform(typeText(inputEnglish));
+        onView(withId(R.id.ptAddGerman)).perform(typeText(inputGerman));
+
+        closeSoftKeyboard();
+
+        onView(withId(R.id.btnAdd)).perform(click());
+
+        onData(anything())
+                .inAdapterView(withId(R.id.lvWordList))
+                .atPosition(0)
+                .check(matches(display))
+    }
+
+    @Test
+    public void testEmptyGermanPtToastVisible() {
+        String inputEnglish = "Apple";
+
+        onView(withId(R.id.ptAddEnglish)).perform(typeText(inputEnglish));
+
+        closeSoftKeyboard();
+
+        onView(withId(R.id.btnAdd)).perform(click());
+
+        AddWordsActivity activity = addWordsActivityTestRule.getActivity();
+        onView(withText(toast)).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).
+                check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testEmptyEnglishPtToastVisible() {
+        String inputGerman = "Apfel";
+
+        onView(withId(R.id.ptAddGerman)).perform(typeText(inputGerman));
+
+        closeSoftKeyboard();
+
+        onView(withId(R.id.btnAdd)).perform(click());
+
+        AddWordsActivity activity = addWordsActivityTestRule.getActivity();
+        onView(withText(toast)).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).
+                check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testEmptyPtsToastVisible() {
+        closeSoftKeyboard();
+
+        onView(withId(R.id.btnAdd)).perform(click());
+
+        AddWordsActivity activity = addWordsActivityTestRule.getActivity();
+        onView(withText(toast)).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).
+                check(matches(isDisplayed()));
     }
 
 }

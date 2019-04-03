c2f480a2f4613705ea2fd9dff2fe5643b2364cc7
Andreas Krueger <andreas.krueger@student.tugraz.at>
2019-04-03 10:04:59 +0200

    LANG_002 Learning interface Implementation  [AK/MB]

--- app/src/main/java/at/tugraz/ist/swe/lang/TranslationActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/lang/TranslationActivity.java
@@ -1,9 +1,12 @@
 package at.tugraz.ist.swe.lang;
 
+import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -16,42 +19,50 @@ import at.tugraz.ist.swe.lang.R;
 
 public class TranslationActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
+    private ArrayAdapter adapter_de;
+    private ArrayAdapter adapter_en;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[]  dewords = getResources().getStringArray(R.array.dewords);
-        String[]  enwords = getResources().getStringArray(R.array.enwords);
+        String[] dewords = getResources().getStringArray(R.array.dewords);
+        String[] enwords = getResources().getStringArray(R.array.enwords);
         Button btn_de = findViewById(R.id.de_btn);
         Button btn_en = findViewById(R.id.en_btn);
 
 
-
-        final ListView listView = (ListView)findViewById(R.id.word_list);
-        final ArrayAdapter<String> adapter_de = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dewords
+        final ListView listView = (ListView) findViewById(R.id.word_list);
+        adapter_de = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dewords
         );
-        final ArrayAdapter<String> adapter_en = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, enwords
+        adapter_en = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, enwords
         );
         listView.setAdapter(adapter_de);
-        listView.setOnItemClickListener(this);
-        btn_de.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        btn_de.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View v){
                 listView.setAdapter(adapter_de);
             }
         });
-        btn_en.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        btn_en.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View v){
                 listView.setAdapter(adapter_en);
             }
         });
+        listView.setOnItemClickListener(this);
 
     }
-
     @Override
     public void onItemClick(AdapterView <?> parent, View view, int arg2, long arg3) {
         TextView text = (TextView)findViewById(R.id.translated_word);
-        String sel_word = parent.getItemAtPosition(arg2).toString().trim();
-        text.setText(sel_word);
+        String[] sel_word;
+        if (parent.getAdapter() == adapter_de) {
+            sel_word = getResources().getStringArray(R.array.enwords);
+        }
+        else
+        {
+            sel_word = getResources().getStringArray(R.array.dewords);
+        }
+        text.setText(sel_word[arg2]);
     }
 }

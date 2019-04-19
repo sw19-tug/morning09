46fb3ecdf629ca43334a7e2770d081a8a2ba2079
mikeberll <m.berlanda@student.tugraz.at>
2019-04-19 14:10:16 +0200

    ListView updates when an element is clicked without checking the correctness of the answer

---
+++ app/src/main/java/at/tugraz/ist/swe/lang/Questions.java
@@ -0,0 +1,57 @@
+package at.tugraz.ist.swe.lang;
+
+public class Questions extends SimpleTestActivity {
+
+    public String myQuestions[] = {
+
+            "Translate the following word: Foot",
+            "Translate the following word: Room",
+            "Translate the following word: Light",
+            "Translate the following word: Eight",
+            "Translate the following word: Mirror",
+            "Translate the following word/phrase: Black Board",
+            "Translate the following word/phrase: Mobi",
+            "Translate the following word: Mirror",
+            "Translate the following word: Mirror",
+            "Translate the following word: Mirror",
+            "Translate the following word: Jacket"
+
+    };
+
+
+    private String myChoices[][] = {
+
+            {"Raum","Fuß","Licht","Spiegel"},
+            {"Acht","Licht","Fuß","Raum"},
+            {"Spiegel","Licht","Raum","Jacke"},
+            {"Acht","Raum","Spiegel","Fuß"},
+            {"Licht","Fuß","Jacke","Spiegel"},
+            {"Jacke","Acht","Spiegel","Raum"},
+
+    };
+
+    private String myCorrectAnswers[] = {"Fuß", "Raum", "Licht", "Acht","Spiegel", "Jacke"};
+
+
+    public String getQuestion (int a){
+
+        String question = myQuestions[a];
+        return question;
+
+    }
+
+    public String[] getItems (int a){
+        String[] items = myChoices[a];
+        return items;
+    }
+
+
+    public String getCorrectAnswer(int a){
+
+        String answer = myCorrectAnswers[a];
+
+        return answer;
+
+    }
+
+}
--- app/src/main/java/at/tugraz/ist/swe/lang/SimpleTestActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/lang/SimpleTestActivity.java
@@ -9,23 +9,61 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.text.BreakIterator;
 import java.util.Random;
 
-public class SimpleTestActivity extends AppCompatActivity {
-
-    TextView score;
-    TextView question;
+public class SimpleTestActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ArrayAdapter adapter;
+    private String myAnswer;
+    private int myScore = 0;
+    private ArrayAdapter answers;
+    private int myQuestionLength;
+    Random r;
+    String[] random_question;
+    //protected Questions myQuestions;
+    TextView score, question;
     ListView multiple;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_test);
 
-        score.findViewById(R.id.score);
-        question.findViewById(R.id.question);
-        multiple.findViewById(R.id.multiple);
+        score = findViewById(R.id.score);
+        question = findViewById(R.id.question);
+        multiple = findViewById(R.id.multiple);
+        Questions myQuestions = new Questions();
+        String[] a = myQuestions.getItems(myScore);
+        score.setText("Score: " + myScore);
+        answers = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,a
+        );
+        multiple.setAdapter(answers);
+        multiple.setOnItemClickListener(this);
     }
 
+    private void updateQuestion(int num) {
+        Questions myQuestions = new Questions();
+        question.setText(myQuestions.getQuestion(num));
+        random_question = myQuestions.getItems(num);
+        myAnswer = myQuestions.getCorrectAnswer(num);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        myScore++;
+        Questions myQuestions = new Questions();
+        String[] a = myQuestions.getItems(myScore);
+        //score.setText("Score: " + myScore);
+        updateArray(a);
+        updateQuestion(myScore);
+    }
+
+    private void updateArray(String[] a) {
+        answers = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,a
+        );
+        multiple.setAdapter(answers);
+        score.setText("Score: " + myScore);
+    }
 }
 

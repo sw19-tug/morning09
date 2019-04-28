8050d849a319b13a3075139f783b42e1c60d5c6f
k.mujic <k.mujic@student.tugraz.at>
2019-04-28 12:34:10 +0200

    LANG_004 Changed textView description [MM, MK]

--- app/src/main/java/at/tugraz/ist/swe/lang/RatingActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/lang/RatingActivity.java
@@ -2,6 +2,7 @@ package at.tugraz.ist.swe.lang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RatingActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class RatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+        TextView text = (TextView)findViewById(R.id.textViewEn);
     }
 }

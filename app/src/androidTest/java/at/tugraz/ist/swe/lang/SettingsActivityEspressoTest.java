64e995612178e635b66cbe8dfcab01512cad8192
Daniel Gärber <daniel.gaerber@student.tugraz.at>
2019-05-22 08:49:32 +0200

    LANG_008 tests for Settings [FT, GD]

---
+++ app/src/androidTest/java/at/tugraz/ist/swe/lang/SettingsActivityEspressoTest.java
@@ -0,0 +1,39 @@
+package at.tugraz.ist.swe.lang;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.ListView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.core.IsNot.not;
+
+@RunWith(AndroidJUnit4.class)
+public class SettingsActivityEspressoTest {
+
+    @Rule
+    public ActivityTestRule<SettingsActivity> settingsActivityTestRule = new ActivityTestRule<>(AddWordsActivity.class);
+
+    @Test
+    public void testElementsVisible() {
+
+        onView(withId(R.id.btnStoreBackup)).check(matches(isDisplayed()));
+        onView(withId(R.id.btnLoadBackup)).check(matches(isDisplayed()));
+
+
+    }
+}

68879bafe6105788ed8a9796e79e2c4cab7225d0
mikeberll <m.berlanda@student.tugraz.at>
2019-05-29 10:02:47 +0200

    LANG_006 Split into 2 activity [MK, BM]

---
+++ app/src/main/java/at/tugraz/ist/swe/lang/CreateTestActivity.java
@@ -0,0 +1,4 @@
+package at.tugraz.ist.swe.lang;
+
+public class CreateTestActivity {
+}
--- app/src/main/java/at/tugraz/ist/swe/lang/TestsActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/lang/TestsActivity.java
@@ -1,5 +1,6 @@
 package at.tugraz.ist.swe.lang;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,28 +30,16 @@ public class TestsActivity extends AppCompatActivity {
         btnCreateTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.activity_test_create);
-                vocabList = (ListView)findViewById(R.id.vocabList);
-                vocabulary = new Vocabulary(getApplicationContext());
-                vocabulary.init();
-                vocabArray = vocabulary.getVocabArray();
-
-                try {
-                    JSONArray jsonArray = vocabulary.getVocabArray();
-                    ArrayList<String> wordArray = new ArrayList<String>();
-
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        String entry = jsonArray.getJSONObject(i).getString("english") + " : " + jsonArray.getJSONObject(i).getString("german");
-                        wordArray.add(entry);
-                    }
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(TestsActivity.this, android.R.layout.simple_list_item_1, wordArray);
-                    vocabList.setAdapter(adapter);
-                }
-                catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                openCreateTest();
             }
         });
+
+
+    }
+
+    private void openCreateTest() {
+        Intent advance_testing = new Intent(this, CreateTestActivity.class);
+        startActivity(advance_testing);
     }
 }
 

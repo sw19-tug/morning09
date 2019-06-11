a89649c53d947caba4487e625627c9c6a44729f8
mikeberll <m.berlanda@student.tugraz.at>
2019-06-11 12:04:03 +0200

    LANG_006 Take test almost done [MK, BM]

--- app/src/main/java/at/tugraz/ist/swe/lang/CreateTestActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/lang/CreateTestActivity.java
@@ -96,5 +96,5 @@ public class CreateTestActivity extends AppCompatActivity {
             }
         });
     }
-    
+
 }
--- app/src/main/java/at/tugraz/ist/swe/lang/TakeTestActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/lang/TakeTestActivity.java
@@ -1,6 +1,9 @@
 package at.tugraz.ist.swe.lang;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,7 +19,7 @@ import org.json.JSONException;
 
 import java.util.ArrayList;
 
-public class TakeTestActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class TakeTestActivity extends AppCompatActivity {
 
     private TextView wordToTest;
     private TextView title_of_page;
@@ -25,10 +28,9 @@ public class TakeTestActivity extends AppCompatActivity implements AdapterView.O
     private Button takeTest;
     private int myScore = 0;
     private int myAttemps = 0;
-    private ArrayAdapter tests;
-    private String[] tests_name;
     ListView tests_list;
     Test test;
+    int counter = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,57 +44,117 @@ public class TakeTestActivity extends AppCompatActivity implements AdapterView.O
 
         try {
             JSONArray jsonArray = test.getVocabArray();
-            ArrayList<String> wordArray = new ArrayList<String>();
+            ArrayList<String> testArray = new ArrayList<String>();
 
             for (int i = 0; i < jsonArray.length(); i++) {
-                String entry = jsonArray.getJSONObject(i).getString("english") + " : " + jsonArray.getJSONObject(i).getString("german");
-                wordArray.add(entry);
+                String title = jsonArray.getJSONObject(i).getString("german");
+                if (title.equals("TITLE:")) {
+                    String entry = jsonArray.getJSONObject(i).getString("english");
+                    testArray.add(entry);
+                }
             }
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(TakeTestActivity.this, android.R.layout.simple_list_item_multiple_choice, wordArray);
-            tests_list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(TakeTestActivity.this, android.R.layout.simple_list_item_single_choice, testArray);
+            tests_list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
             tests_list.setAdapter(adapter);
         }
         catch (JSONException e) {
             e.printStackTrace();
         }
-        //tests_name = test.findTitles();
-        //tests = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,tests_name
-        //);
-        //tests_list.setAdapter(tests);
-        //tests_list.setOnItemClickListener(this);
 
-
-
-
-
-        /*wordToTest = findViewById(R.id.word_to_test);
-        testedWord = findViewById(R.id.tested_word);
-        tryTest = findViewById(R.id.try_test);
-
-        wordToTest.setText("Try");
-
-        tryTest.setOnClickListener(new Button.OnClickListener() {
+        takeTest.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myAttemps++;
-                String word1 = wordToTest.getText().toString();
-                String word2 = testedWord.getText().toString();
-                if (word1.equals(word2))
-                {
-                    myScore++;
-                    Toast.makeText(TakeTestActivity.this,"Well done!!! You passed your " +
-                            "exam with: " +myScore + " Correct Answers in " +myAttemps + " Attemps", Toast.LENGTH_LONG).show();
-                }
-                else
-                {
-                    Toast.makeText(TakeTestActivity.this,"Failed", Toast.LENGTH_LONG).show();
+                String title_ot_test = (String) tests_list.getAdapter().getItem(tests_list.getCheckedItemPosition());
+
+                try {
+                    JSONArray jsonArray = test.getVocabArray();
+                    ArrayList<String> wordArray = new ArrayList<String>();
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        String title = jsonArray.getJSONObject(i).getString("german");
+                        if (title.equals("TITLE:")) {
+                            String entry = jsonArray.getJSONObject(i).getString("english");
+                            if (entry.equals(title_ot_test)) {
+                                String rrr;
+                                i++;
+                                while (jsonArray.getJSONObject(i).getString("german").equals("TITLE:")) {
+
+                                    rrr = jsonArray.getJSONObject(i).getString("english") + " : " + jsonArray.getJSONObject(i).getString("german");
+                                    wordArray.add(rrr);
+                                    i++;
+                                    }
+                                break;
+                            }
+
+
+                            }
+
+                        }
+                    Toast.makeText(TakeTestActivity.this, "Start test " + title_ot_test + " with " + wordArray.size() + " questions.", Toast.LENGTH_LONG).show();
+                    performTest(wordArray);
+                    }
+                catch (JSONException e) {
+                    e.printStackTrace();
                 }
             }
-        }); */
+        });
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    private void performTest(ArrayList<String> wordArray) {
 
+        setContentView(R.layout.activity_take_test);
+        wordToTest = findViewById(R.id.word_to_test);
+        testedWord = (EditText)findViewById((R.id.tested_word));
+        tryTest = findViewById(R.id.try_test);
+
+        while (wordArray.size() > counter) {
+            final String[] words = wordArray.get(counter).split(" : ");
+            wordToTest.setText(words[0]);
+            tryTest.setOnClickListener(new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (testedWord.getText().toString().equals(words[1])) {
+                        counter++;
+                        myScore++;
+                    }
+                    else
+                    {
+                        Toast.makeText(TakeTestActivity.this, "Error!", Toast.LENGTH_LONG).show();
+                    }
+                    myAttemps++;
+                }
+            });
+        }
+        youWin();
     }
+
+    private void youWin() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(TakeTestActivity.this);
+        alertDialogBuilder
+                .setMessage("Well done!!! You passed your exam with: " +myScore + " Correct Answers in " +myAttemps + " Attemps")
+                .setCancelable(false)
+                .setPositiveButton("Retry?",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                startActivity(new Intent(getApplicationContext(),TakeTestActivity.class));
+                                finish();
+
+                            }
+                        })
+                .setNegativeButton("Exit",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                finish();
+
+                            }
+                        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+
 }
